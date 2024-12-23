4d0f2a49ba09c784893ad97e307c5f87d1963438
Gokulraj002 <gokulrj002@gmail.com>
2024-12-23 10:01:20 +0530

    saas

--- src/Component/Resources/Blogs/BlogImage.js
+++ src/Component/Resources/Blogs/BlogImage.js
@@ -1,9 +1,9 @@
-import blog1 from"./Images/voice1.jpg";
-import blog2 from"./Images/blog1.jpg";
+import blog1 from"./Images/How WhatsApp Chatbots Can Revolutionize Customer Support for Indian SMEs.jpg";
+// import blog2 from"./Images/blog1.jpg";
 
 const BlogImages = {
   1: blog1,
-  2: blog2,
+  // 2: blog2,
 };
 
 export default BlogImages;
